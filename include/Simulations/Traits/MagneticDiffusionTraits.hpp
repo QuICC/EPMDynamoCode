150e7f59db08d9f59a6449296b2f724d5e0e398d
Philippe Marti ETHZ <philippe.marti@erdw.ethz.ch>
2010-06-18 07:27:38 +0200

    Added some simulation type traits

---
+++ include/Simulations/Traits/MagneticDiffusionTraits.hpp
@@ -0,0 +1,52 @@
+/** \file MagneticDiffusionTraits.hpp
+ *  \brief Traits for the dynamo simulation implementation
+ */
+
+#ifndef MAGNETICDIFFUSIONTRAITS_HPP
+#define MAGNETICDIFFUSIONTRAITS_HPP
+
+// System includes
+//
+
+// External includes
+//
+
+// Project includes
+//
+#include "General/EPMTypedefs.hpp"
+#include "PhysicalFields/CodensityScalar.hpp"
+#include "PhysicalFields/MagneticField.hpp"
+#include "PhysicalFields/VelocityField.hpp"
+
+namespace EPMDynamo {
+
+   /**
+    * @brief Dynamo traits class
+    *
+    * \tparam TSimType Type of the simulation
+    */
+   template <typename TSimType> class MagneticDiffusionTraits
+   {
+      public:
+         /// Typedef for the codensity scalar type
+         typedef CodensityScalar<TSimType>  CodType;
+
+         /// Typedef for the magnetic field type
+         typedef MagneticField<TSimType>  MagType;
+
+         /// Typedef for the velocity field type
+         typedef VelocityField<TSimType>   VelType; 
+
+         /// Does simulation required a codensity field
+         static const bool NeedCodensity = false;
+
+         /// Does simulation required a magnetic field
+         static const bool NeedMagnetic = true;
+
+         /// Does simulation required a velocity field
+         static const bool NeedVelocity = false;
+   };
+
+}
+
+#endif // MAGNETICDIFFUSIONTRAITS_HPP
---
+++ include/Simulations/Traits/ThermalConvectionTraits.hpp
@@ -0,0 +1,52 @@
+/** \file ThermalConvectionTraits.hpp
+ *  \brief Traits for the dynamo simulation implementation
+ */
+
+#ifndef THERMALCONVECTIONTRAITS_HPP
+#define THERMALCONVECTIONTRAITS_HPP
+
+// System includes
+//
+
+// External includes
+//
+
+// Project includes
+//
+#include "General/EPMTypedefs.hpp"
+#include "PhysicalFields/CodensityScalar.hpp"
+#include "PhysicalFields/MagneticField.hpp"
+#include "PhysicalFields/VelocityField.hpp"
+
+namespace EPMDynamo {
+
+   /**
+    * @brief Dynamo traits class
+    *
+    * \tparam TSimType Type of the simulation
+    */
+   template <typename TSimType> class ThermalConvectionTraits
+   {
+      public:
+         /// Typedef for the codensity scalar type
+         typedef CodensityScalar<TSimType>  CodType;
+
+         /// Typedef for the magnetic field type
+         typedef MagneticField<TSimType>  MagType;
+
+         /// Typedef for the velocity field type
+         typedef VelocityField<TSimType>   VelType; 
+
+         /// Does simulation required a codensity field
+         static const bool NeedCodensity = true;
+
+         /// Does simulation required a magnetic field
+         static const bool NeedMagnetic = false;
+
+         /// Does simulation required a velocity field
+         static const bool NeedVelocity = true;
+   };
+
+}
+
+#endif // THERMALCONVECTIONTRAITS_HPP
