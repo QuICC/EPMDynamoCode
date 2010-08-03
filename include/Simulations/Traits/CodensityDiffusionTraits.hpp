f996a4f373fe3e0564fdfe9b98858edfbde75933
Philippe Marti rohan <philippe.marti@gmail.com>
2010-08-03 08:54:12 +0200

    Added new information to all traits

--- include/Simulations/Traits/CodensityDiffusionTraits.hpp
+++ include/Simulations/Traits/CodensityDiffusionTraits.hpp
@@ -31,11 +31,11 @@ namespace EPMDynamo {
          /// Typedef for the codensity scalar type
          typedef CodensityScalar<TSimType>  CodType;
 
-         /// Typedef for the magnetic field type
-         typedef MagneticField<TSimType>  MagType;
+         /// Does the codensity field have an IC source ?
+         static const bool HasCodICSource = false;
 
-         /// Typedef for the velocity field type
-         typedef VelocityField<TSimType>   VelType; 
+         /// Does the codensity field have an OC source ?
+         static const bool HasCodOCSource = true;
 
          /// Does simulation required a codensity field
          static const bool NeedCodensity = true;
@@ -43,11 +43,33 @@ namespace EPMDynamo {
          /// Does simulation required a codensity gradient field
          static const bool NeedCodensityGrad = false;
 
+
+
+         /// Typedef for the magnetic field type
+         typedef MagneticField<TSimType>  MagType;
+
+         /// Does the magnetic field have an IC source ?
+         static const bool HasMagICSource = false;
+
+         /// Does the magnetic field have an OC source ?
+         static const bool HasMagOCSource = false;
+
          /// Does simulation required a magnetic field
          static const bool NeedMagnetic = false;
 
          /// Does simulation required a magnetic curl field
          static const bool NeedMagneticCurl = false;
+
+
+
+         /// Typedef for the velocity field type
+         typedef VelocityField<TSimType>   VelType; 
+
+         /// Does the velocity field have an IC source ?
+         static const bool HasVelICSource = false;
+
+         /// Does the velocity field have an OC source ?
+         static const bool HasVelOCSource = false;
 
          /// Does simulation required a velocity field
          static const bool NeedVelocity = false;
--- include/Simulations/Traits/ThermalConvectionTraits.hpp
+++ include/Simulations/Traits/ThermalConvectionTraits.hpp
@@ -37,6 +37,14 @@ namespace EPMDynamo {
          /// Does the codensity field have an OC source ?
          static const bool HasCodOCSource = true;
 
+         /// Does simulation required a codensity field
+         static const bool NeedCodensity = true;
+
+         /// Does simulation required a codensity gradient field
+         static const bool NeedCodensityGrad = true;
+
+
+
          /// Typedef for the magnetic field type
          typedef MagneticField<TSimType>  MagType;
 
@@ -46,6 +54,14 @@ namespace EPMDynamo {
          /// Does the magnetic field have an OC source ?
          static const bool HasMagOCSource = false;
 
+         /// Does simulation required a magnetic field
+         static const bool NeedMagnetic = false;
+
+         /// Does simulation required a magnetic curl field
+         static const bool NeedMagneticCurl = false;
+
+
+
          /// Typedef for the velocity field type
          typedef VelocityField<TSimType>   VelType; 
 
@@ -54,18 +70,6 @@ namespace EPMDynamo {
 
          /// Does the velocity field have an OC source ?
          static const bool HasVelOCSource = false;
-
-         /// Does simulation required a codensity field
-         static const bool NeedCodensity = true;
-
-         /// Does simulation required a codensity gradient field
-         static const bool NeedCodensityGrad = true;
-
-         /// Does simulation required a magnetic field
-         static const bool NeedMagnetic = false;
-
-         /// Does simulation required a magnetic curl field
-         static const bool NeedMagneticCurl = false;
 
          /// Does simulation required a velocity field
          static const bool NeedVelocity = true;
