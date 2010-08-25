fc351fef26bcc43b2ea6a3b609531fcbdc13bae8
Philippe Marti <pmarti@ela1.login.cscs.ch>
2010-08-25 11:04:23 +0200

    Crappy implementation of some timings

--- include/Simulations/Simulation.hpp
+++ include/Simulations/Simulation.hpp
@@ -140,9 +140,20 @@ namespace EPMDynamo {
       // Execute last initialisation before run
       this->preRun();
 
+EPMFloat t_upRTP(0.0);
+EPMFloat t_upRHS(0.0);
+EPMFloat t_trRHS(0.0);
+EPMFloat t_exInf(0.0);
+EPMFloat t_upTim(0.0);
+EPMFloat t_tiEqu(0.0);
+EPMFloat t_siUpd(0.0);
+EPMFloat t_wrFil(0.0);
+EPMFloat t_sync(0.0);
+
       // Start looping as long as requested
       while(this->mSimControl.keepRunning())
       {
+this->mTimer.start();
          // Update RHS of all equation
          for(int i=0; i < this->mTransformSteps; ++i)
          {
@@ -150,10 +161,16 @@ namespace EPMDynamo {
 
             this->updateEquationsRTP(i);
          }
+this->mTimer.stop();
+t_upRTP += this->mTimer.time();
 
+this->mTimer.start();
          // Update RHS of all equation
          this->updateEquationsRHS();
+this->mTimer.stop();
+t_upRHS += this->mTimer.time();
 
+this->mTimer.start();
          // Transform RHS to spectral space
          for(int i=0; i < this->mTransformSteps; ++i)
          {
@@ -161,25 +178,71 @@ namespace EPMDynamo {
 
             this->transformEquationsRHS(i);
          }
+this->mTimer.stop();
+t_trRHS += this->mTimer.time();
 
+this->mTimer.start();
          // Include possible source terms / imposed fields
          this->addExternalInfluence();
+this->mTimer.stop();
+t_exInf += this->mTimer.time();
 
+this->mTimer.start();
          // Update the timestep
          this->mSimControl.tsControl().updateTimestep();
+this->mTimer.stop();
+t_upTim += this->mTimer.time();
 
+this->mTimer.start();
          // Timestep all equations
          this->timestepEquations();
+this->mTimer.stop();
+t_tiEqu += this->mTimer.time();
 
+this->mTimer.start();
          // Update other values of simulation control
          this->mSimControl.update();
+this->mTimer.stop();
+t_siUpd += this->mTimer.time();
 
+this->mTimer.start();
          // Save files
          this->writeFiles();
+this->mTimer.stop();
+t_wrFil += this->mTimer.time();
 
+this->mTimer.start();
          // Synchronize simulation
          EPMDYNAMO_SYNCHRONIZE;
+this->mTimer.stop();
+t_sync += this->mTimer.time();
       }
+
+std::ofstream  file;
+std::ostringstream oss;
+oss << EPMDYNAMO_RANK;
+file.open("sim_timings"+oss.str()+".dat");
+file << t_upRTP << std::endl;
+file << t_upRHS << std::endl;
+file << t_trRHS << std::endl;
+file << t_exInf << std::endl;
+file << t_upTim << std::endl;
+file << t_tiEqu << std::endl;
+file << t_siUpd << std::endl;
+file << t_wrFil << std::endl;
+file << t_sync << std::endl;
+
+file << std::endl << std::endl << std::endl;
+file << t_upRTP << "\t";
+file << t_upRHS << "\t";
+file << t_trRHS << "\t";
+file << t_exInf << "\t";
+file << t_upTim << "\t";
+file << t_tiEqu << "\t";
+file << t_siUpd << "\t";
+file << t_wrFil << "\t";
+file << t_sync << std::endl;
+file.close();
 
       // Execute last initialisation before end
       this->postRun();
