ef147c9dd4a604c7bee34073408403e9ab079924
Philippe Marti rohan <philippe.marti@gmail.com>
2010-12-03 23:21:15 +0100

    Added the missing one (IMPLEMENTATION IS WRONG)

---
+++ include/Simulations/Implementations/LinearMagnetoConvectionSimulation.hpp
@@ -0,0 +1,364 @@
+/** \file LinearMagnetoConvectionSimulation.hpp
+ *  \brief Implementation of the linear magneto-convection simulation
+ */
+
+#ifndef LINEARMAGNETOCONVECTIONSIMULATION_HPP
+#define LINEARMAGNETOCONVECTIONSIMULATION_HPP
+
+// Configuration includes
+//
+#include "Config/SmartPointer.h"
+
+// System includes
+//
+
+// External includes
+//
+
+// Project includes
+//
+#include "General/EPMTypedefs.hpp"
+#include "Simulations/SimulationBase.hpp"
+#include "Simulations/Traits/LinearMagnetoConvectionTraits.hpp"
+
+#include "IO/HDF5/Imposed/ImposedFieldReader.hpp"
+#include "IO/HDF5/State/StateFileReader.hpp"
+#include "IO/HDF5/State/StateFileWriter.hpp"
+#include "IO/HDF5/Source/SourceFileReader.hpp"
+#include "IO/ASCII/EnergyFile.hpp"
+#include "IO/ASCII/SpectrumFile.hpp"
+
+#include "Equations/Induction/InductionMHD.hpp"
+#include "Equations/NavierStokes/NavierStokesMagnetic.hpp"
+#include "Equations/Transport/TransportMHD.hpp"
+
+#include "BoundaryConditions/Homogeneous/ZeroBC.hpp"
+#include "BoundaryConditions/Homogeneous/DDRadialBC.hpp"
+#include "BoundaryConditions/Homogeneous/DRadialBC.hpp"
+#include "BoundaryConditions/Homogeneous/InsulatingBC.hpp"
+#include "BoundaryConditions/Homogeneous/StressFreeTorBC.hpp"
+
+namespace EPMDynamo {
+
+   /**
+    * \brief Implementation of the linear magneto-convection simulation
+    *
+    * \tparam TSimType Type of the simulation
+    */
+   template <typename TSimType> class LinearMagnetoConvectionSimulation: public SimulationBase<TSimType>
+   {
+      public:
+         /**
+          * @brief Simple empty destructor
+          */
+         virtual ~LinearMagnetoConvectionSimulation() {};
+
+      protected:
+         /**
+          * @brief Constructor
+          */
+         LinearMagnetoConvectionSimulation();
+
+         /**
+          * @brief Initialise the fields
+          */
+         void initFields();
+
+         /**
+          * @brief Add HDF5 output files
+          */
+         void addHDF5Output();
+
+         /**
+          * @brief Add ASCII output files
+          */
+         void addASCIIOutput();
+
+         /**
+          * @brief Initialise the equations
+          */
+         void initEquations();
+
+         /**
+          * @brief Update equations RTP values
+          *
+          * \param step Current step in a multistep transform
+          */
+         void updateEquationsRTP(const int step);
+
+         /**
+          * @brief Update equations RHS
+          */
+         void updateEquationsRHS();
+
+         /**
+          * @brief Transform equations RHS
+          *
+          * \param step Current step in a multistep transform
+          */
+         void transformEquationsRHS(const int step);
+
+         /**
+          * @brief Include external influence
+          */
+         void addExternalInfluence();
+
+         /**
+          * @brief Timestep the equations
+          */
+         void timestepEquations();
+
+         /**
+          * @brief Configure the transforms
+          */
+         void configureTransforms();
+
+      private:
+         /**
+          * @brief Angular distance factor for CFL condition
+          */
+         EPMFloat mCFLFactor;
+
+         /**
+          * @brief Codensity scalar
+          */
+         typename LinearMagnetoConvectionTraits<TSimType>::CodType   mCodC;
+
+         /**
+          * @brief Magnetic field
+          */
+         typename LinearMagnetoConvectionTraits<TSimType>::MagType   mMagB;
+
+         /**
+          * @brief Velocity field
+          */
+         typename LinearMagnetoConvectionTraits<TSimType>::VelType   mVelV;
+
+         /**
+          * @brief Induction equation
+          */
+         InductionMHD<TSimType, LinearMagnetoConvectionTraits>    mInduction;
+
+         /**
+          * @brief Transport equation
+          */
+         TransportMHD<TSimType, LinearMagnetoConvectionTraits>    mTransport;
+
+         /**
+          * @brief Navier Stokes equation
+          */
+         NavierStokesMagnetic<TSimType, LinearMagnetoConvectionTraits>    mNavierStokes;
+   };
+
+   template <typename TSimType> LinearMagnetoConvectionSimulation<TSimType>::LinearMagnetoConvectionSimulation()
+      : mCFLFactor(1.0), mCodC(this->mpTrunc, this->mTransform), mMagB(this->mpTrunc, this->mTransform), mVelV(this->mpTrunc, this->mTransform), mInduction(mMagB, mVelV, this->mTransform, this->mSimControl.tsParams(), this->mEqParams), mTransport(mCodC, mVelV, this->mTransform, this->mSimControl.tsParams(), this->mEqParams), mNavierStokes(mVelV, mMagB, this->mTransform, this->mSimControl.tsParams(), this->mEqParams)
+   {
+      // Set the CFL factor to L*(L+1)
+      int l = this->mpTrunc->sim()->hoz()->nL();
+      this->mCFLFactor = static_cast<EPMFloat>(l*(l+1));
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::initEquations()
+   {
+      // Set boundary condition to the transport equation
+      SmartBC  pZeroBCC(new ZeroBC<TSimType>(this->mTransform.radBasis()));
+      //this->mTransport.addBC(pDiffHeatBC);
+      this->mTransport.addBC(pZeroBCC);
+
+      // Set boundary condition to the Induction equation
+      SmartBC  pZeroBC(new ZeroBC<TSimType>(this->mTransform.radBasis()));
+      SmartBC  pInsulatingBC(new InsulatingBC<TSimType>(this->mTransform.radBasis()));
+      mInduction.addTorBC(pZeroBC);
+      mInduction.addPolBC(pInsulatingBC);
+
+      // Set boundary condition to the Navier-Stokes equation
+      //SmartBC  pSFBC(new StressFreeTorBC<TSimType>(this->mTransform.radBasis()));
+      SmartBC  pNSBC(new ZeroBC<TSimType>(this->mTransform.radBasis()));
+      //SmartBC  pDDBC(new DDRadialBC<TSimType>(this->mTransform.radBasis()));
+      SmartBC  pDBC(new DRadialBC<TSimType>(this->mTransform.radBasis()));
+      this->mNavierStokes.addTorBC(pNSBC);
+      // Order of Poloidal BCs is important
+      this->mNavierStokes.addPolBC(pZeroBC);
+      this->mNavierStokes.addPolBC(pDBC);
+
+      // Initialise the induction equation
+      this->mInduction.init();
+
+      // Initialise the transport equation
+      this->mTransport.init();
+
+      // Initialise the Navier-Stokes equation
+      this->mNavierStokes.init();
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::updateEquationsRTP(const int step)
+   {
+      // Update RTP values of the induction equation
+      this->mInduction.updateRTP(step);
+
+      // Update RTP values of the transport equation
+      this->mTransport.updateRTP(step);
+
+      // Update RTP values of the Navier-Stokes equation
+      this->mNavierStokes.updateRTP(step);
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::updateEquationsRHS()
+   {
+      // Update RHS of the induction equation
+      this->mInduction.updateRHS();
+
+      // Update RHS of the transport equation
+      this->mTransport.updateRHS();
+
+      // Update RHS of the Navier-Stokes equation
+      this->mNavierStokes.updateRHS();
+
+      // Update the CFL timestep condition
+      this->mSimControl.tsControl().updateCFLTimestep(mMagB.oc().rtp(), mVelV.oc().rtp(), mCFLFactor);
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::transformEquationsRHS(const int step)
+   {
+      // Transform RHS of the induction equation
+      mInduction.transformRHS(step);
+
+      // Transform RHS of the transport equation
+      this->mTransport.transformRHS(step);
+
+      // Transform RHS of the Navier-Stokes equation
+      mNavierStokes.transformRHS(step);
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::addExternalInfluence()
+   {
+      // Add Thermal source term
+      this->mTransport.addSourceTerm();
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::timestepEquations()
+   {
+      // Timestep the induction equation
+      mInduction.timestep();
+
+      // Timestep the transport equation
+      this->mTransport.timestep();
+
+      // Timestep the Navier-Stokes equation
+      mNavierStokes.timestep();
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::configureTransforms()
+   {
+      //
+      // Setup the SSH transform data manipulator
+      //
+      
+      // Register transform data packs of induction equation for SSH
+      this->registerSSHPacks(this->mInduction.nFSSHPacks(), this->mInduction.nSSHBPacks());
+
+      // Register transform data packs of transport equation for SSH
+      this->registerSSHPacks(this->mTransport.nFSSHPacks(), this->mTransport.nSSHBPacks());
+
+      // Register transform data packs of navier stokes equation for SSH
+      this->registerSSHPacks(this->mNavierStokes.nFSSHPacks(), this->mNavierStokes.nSSHBPacks());
+
+      // Configure the SSH manipulator
+      this->configureSSHManipulator();
+
+      //
+      // Setup the SH transform data manipulator
+      //
+      
+      // Register transform data packs of induction equation for SH
+      this->registerSHPacks(this->mInduction.nFSHPacks(), this->mInduction.nSHBPacks());
+
+      // Register transform data packs of transport equation for SH
+      this->registerSHPacks(this->mTransport.nFSHPacks(), this->mTransport.nSHBPacks());
+
+      // Register transform data packs of navier stokes equation for SH
+      this->registerSHPacks(this->mNavierStokes.nFSHPacks(), this->mNavierStokes.nSHBPacks());
+
+      // Configure the SSH manipulator
+      this->configureSHManipulator();
+
+      //
+      // Configure transform nesting
+      //
+
+      // Configure the transforms' nesting setup
+      this->configureTransformNesting();
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::initFields()
+   {
+      // Read in initial state
+      EPMSHARED_PTR<StateFileReader<TSimType, LinearMagnetoConvectionTraits> > pInState(new StateFileReader<TSimType, LinearMagnetoConvectionTraits>(this->mCodC, this->mMagB, this->mVelV,  "_initial"));
+
+      this->mIOSys.useInitialState(pInState, this->mSimControl.tsParams());
+
+      // Create a source file reader for the codensity source
+      EPMSHARED_PTR<SourceFileReader<TSimType, LinearMagnetoConvectionTraits> > pSource(new SourceFileReader<TSimType, LinearMagnetoConvectionTraits>(this->mCodC));
+
+      // Read in source state
+      this->mIOSys.useSource(pSource);
+
+      // Rescale codensity source
+      this->mCodC.rOcSrc().rescale(this->mEqParams.codSourceScale());
+
+      // Read in imposed magnetic field
+      EPMSHARED_PTR<ImposedFieldReader<TSimType, LinearMagnetoConvectionTraits> > pIField(new ImposedFieldReader<TSimType, LinearMagnetoConvectionTraits>(this->mMagB));
+
+      this->mIOSys.useImposedField(pIField);
+
+      // Scale imposed field to correct values
+      this->mMagB.rOc().rImposed().rescale(this->mEqParams.imposedMagScale());
+
+      // Set the energy scale for the magnetic field
+      this->mMagB.rOc().rPerturbation().setEnergyScale(this->mEqParams.meFactor());
+
+      // Set the energy scale for the velocity field
+      this->mVelV.rOc().rPerturbation().setEnergyScale(this->mEqParams.keFactor());
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::addHDF5Output()
+   {
+      EPMSHARED_PTR<StateFileWriter<TSimType, LinearMagnetoConvectionTraits> >  pOutState(new StateFileWriter<TSimType, LinearMagnetoConvectionTraits>(this->mCodC, this->mMagB, this->mVelV, this->mEqParams, this->mSimControl.tsParams()));
+
+      this->mIOSys.addHDF5Writer(pOutState);
+   }
+
+   template <typename TSimType> void LinearMagnetoConvectionSimulation<TSimType>::addASCIIOutput()
+   {
+      // Create a energy ASCII diagnostic file for the codensity scalar
+      EPMSHARED_PTR<EnergyFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::CodType> > pCodEnergy(new EnergyFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::CodType>(this->mCodC, "cod", this->mSimControl.tsParams()));
+      // Create a energy ASCII diagnostic file for the magnetic field
+      EPMSHARED_PTR<EnergyFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::MagType> > pMagEnergy(new EnergyFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::MagType>(this->mMagB, "mag", this->mSimControl.tsParams()));
+      // Create a energy ASCII diagnostic file for the velocity field
+      EPMSHARED_PTR<EnergyFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::VelType> > pVelEnergy(new EnergyFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::VelType>(this->mVelV, "vel", this->mSimControl.tsParams()));
+
+      // Add codensity energy to ASCII output
+      this->mIOSys.addASCIIWriter(pCodEnergy);
+      // Add magnetic energy to ASCII output
+      this->mIOSys.addASCIIWriter(pMagEnergy);
+      // Add kinetic energy to ASCII output
+      this->mIOSys.addASCIIWriter(pVelEnergy);
+
+      // Create a energy spectrum ASCII diagnostic file for the codensity scalar
+      EPMSHARED_PTR<SpectrumFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::CodType> > pCodSpectrum(new SpectrumFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::CodType>(this->mCodC, "cod"));
+      // Create a energy spectrum ASCII diagnostic file for the magnetic field
+      EPMSHARED_PTR<SpectrumFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::MagType> > pMagSpectrum(new SpectrumFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::MagType>(this->mMagB, "mag"));
+      // Create a energy spectrum ASCII diagnostic file for the velocity field
+      EPMSHARED_PTR<SpectrumFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::VelType> > pVelSpectrum(new SpectrumFile<TSimType, typename LinearMagnetoConvectionTraits<TSimType>::VelType>(this->mVelV, "vel"));
+
+      // Add codensity energy spectrum to ASCII output
+      this->mIOSys.addASCIIWriter(pCodSpectrum);
+      // Add magnetic energy spectrum to ASCII output
+      this->mIOSys.addASCIIWriter(pMagSpectrum);
+      // Add kinetic energy spectrum to ASCII output
+      this->mIOSys.addASCIIWriter(pVelSpectrum);
+   }
+
+}
+
+#endif // LINEARMAGNETOCONVECTIONSIMULATION_HPP
---
+++ include/Simulations/Implementations/LinearRotatingConvectionSimulation.hpp
@@ -0,0 +1,307 @@
+/** \file LinearRotatingConvectionSimulation.hpp
+ *  \brief Implementation of a linear rotating convection simulation
+ */
+
+#ifndef LINEARROTATINGCONVECTIONSIMULATION_HPP
+#define LINEARROTATINGCONVECTIONSIMULATION_HPP
+
+// Configuration includes
+//
+#include "Config/SmartPointer.h"
+
+// System includes
+//
+
+// External includes
+//
+
+// Project includes
+//
+#include "General/EPMTypedefs.hpp"
+#include "Simulations/SimulationBase.hpp"
+#include "Simulations/Traits/LinearRotatingConvectionTraits.hpp"
+
+#include "IO/HDF5/State/StateFileReader.hpp"
+#include "IO/HDF5/State/StateFileWriter.hpp"
+#include "IO/HDF5/Source/SourceFileReader.hpp"
+#include "IO/ASCII/EnergyFile.hpp"
+#include "IO/ASCII/SpectrumFile.hpp"
+#include "IO/ASCII/TimeFile.hpp"
+
+#include "Equations/Transport/TransportMHD.hpp"
+#include "Equations/NavierStokes/NavierStokesLinThermal.hpp"
+
+#include "BoundaryConditions/Homogeneous/ZeroBC.hpp"
+#include "BoundaryConditions/Homogeneous/DDRadialBC.hpp"
+#include "BoundaryConditions/Homogeneous/StressFreeTorBC.hpp"
+
+namespace EPMDynamo {
+
+   /**
+    * \brief Implementation of a linear rotating convection simulation
+    *
+    * \tparam TSimType Type of the simulation
+    */
+   template <typename TSimType> class LinearRotatingConvectionSimulation: public SimulationBase<TSimType>
+   {
+      public:
+         /**
+          * @brief Simple empty destructor
+          */
+         virtual ~LinearRotatingConvectionSimulation() {};
+
+      protected:
+         /**
+          * @brief Constructor
+          */
+         LinearRotatingConvectionSimulation();
+
+         /**
+          * @brief Initialise the fields
+          */
+         void initFields();
+
+         /**
+          * @brief Add HDF5 output files
+          */
+         void addHDF5Output();
+
+         /**
+          * @brief Add ASCII output files
+          */
+         void addASCIIOutput();
+
+         /**
+          * @brief Initialise the equations
+          */
+         void initEquations();
+
+         /**
+          * @brief Update equations RTP values
+          *
+          * \param step Current step in a multistep transform
+          */
+         void updateEquationsRTP(const int step);
+
+         /**
+          * @brief Update equations RHS
+          */
+         void updateEquationsRHS();
+
+         /**
+          * @brief Transform equations RHS
+          *
+          * \param step Current step in a multistep transform
+          */
+         void transformEquationsRHS(const int step);
+
+         /**
+          * @brief Include external influence
+          */
+         void addExternalInfluence();
+
+         /**
+          * @brief Timestep the equations
+          */
+         void timestepEquations();
+
+         /**
+          * @brief Configure the transforms
+          */
+         void configureTransforms();
+
+      private:
+         /**
+          * @brief Angular distance factor for CFL condition
+          */
+         EPMFloat mCFLFactor;
+
+         /**
+          * @brief Codensity scalar
+          */
+         typename LinearRotatingConvectionTraits<TSimType>::CodType   mCodC;
+
+         /**
+          * @brief Velocity field
+          */
+         typename LinearRotatingConvectionTraits<TSimType>::VelType   mVelV;
+
+         /**
+          * @brief Transport equation
+          */
+         TransportMHD<TSimType, LinearRotatingConvectionTraits>    mTransport;
+
+         /**
+          * @brief Navier Stokes equation
+          */
+         NavierStokesLinThermal<TSimType, LinearRotatingConvectionTraits>    mNavierStokes;
+   };
+
+   template <typename TSimType> LinearRotatingConvectionSimulation<TSimType>::LinearRotatingConvectionSimulation()
+      : mCFLFactor(1.0), mCodC(this->mpTrunc, this->mTransform), mVelV(this->mpTrunc, this->mTransform), mTransport(mCodC, mVelV, this->mTransform, this->mSimControl.tsParams(), this->mEqParams), mNavierStokes(mVelV, mCodC, this->mTransform, this->mSimControl.tsParams(), this->mEqParams)
+   {
+      // Set the CFL factor to L*(L+1)
+      int l = this->mpTrunc->sim()->hoz()->nL();
+      this->mCFLFactor = static_cast<EPMFloat>(l*(l+1));
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::initEquations()
+   {
+      // Set boundary condition to the transport equation
+      SmartBC  pZeroBC(new ZeroBC<TSimType>(this->mTransform.radBasis()));
+      this->mTransport.addBC(pZeroBC);
+
+      // Set boundary condition to the Navier-Stokes equation
+      SmartBC  pSFBC(new StressFreeTorBC<TSimType>(this->mTransform.radBasis()));
+      SmartBC  pDDBC(new DDRadialBC<TSimType>(this->mTransform.radBasis()));
+      this->mNavierStokes.addTorBC(pSFBC);
+      // Order of Poloidal BCs is important
+      this->mNavierStokes.addPolBC(pZeroBC);
+      this->mNavierStokes.addPolBC(pDDBC);
+
+      // Initialise the transport equation
+      this->mTransport.init();
+
+      // Initialise the Navier-Stokes equation
+      this->mNavierStokes.init();
+
+      TimestepConfig::overruleMaxTimestep(1e-5);
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::configureTransforms()
+   {
+      //
+      // Setup the SSH transform data manipulator
+      //
+
+      // Register transform data packs of transport equation for SSH
+      this->registerSSHPacks(this->mTransport.nFSSHPacks(), this->mTransport.nSSHBPacks());
+
+      // Register transform data packs of navier stokes equation for SSH
+      this->registerSSHPacks(this->mNavierStokes.nFSSHPacks(), this->mNavierStokes.nSSHBPacks());
+
+      // Configure the SSH manipulator
+      this->configureSSHManipulator();
+
+      //
+      // Setup the SH transform data manipulator
+      //
+      
+      // Register transform data packs of transport equation for SH
+      this->registerSHPacks(this->mTransport.nFSHPacks(), this->mTransport.nSHBPacks());
+
+      // Register transform data packs of navier stokes equation for SH
+      this->registerSHPacks(this->mNavierStokes.nFSHPacks(), this->mNavierStokes.nSHBPacks());
+
+      // Configure the SSH manipulator
+      this->configureSHManipulator();
+
+      //
+      // Configure transform nesting
+      //
+
+      // Configure the transforms' nesting setup
+      this->configureTransformNesting();
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::updateEquationsRTP(const int step)
+   {
+      // Update RTP values of the transport equation
+      this->mTransport.updateRTP(step);
+
+      // Update RTP values of the Navier-Stokes equation
+      this->mNavierStokes.updateRTP(step);
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::updateEquationsRHS()
+   {
+      // Update RHS of the transport equation
+      this->mTransport.updateRHS();
+
+      // Update RHS of the Navier-Stokes equation
+      this->mNavierStokes.updateRHS();
+
+      // Update the CFL timestep condition
+      this->mSimControl.tsControl().updateCFLTimestep(this->mVelV.oc().rtp(), this->mCFLFactor);
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::transformEquationsRHS(const int step)
+   {
+      // Update RHS of the transport equation
+      this->mTransport.transformRHS(step);
+
+      // Update RHS of the Navier-Stokes equation
+      this->mNavierStokes.transformRHS(step);
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::addExternalInfluence()
+   {
+      // Add Thermal source term
+      this->mTransport.addSourceTerm();
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::timestepEquations()
+   {
+      // Timestep the transport equation
+      this->mTransport.timestep();
+
+      // Timestep the Navier-Stokes equation
+      this->mNavierStokes.timestep();
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::initFields()
+   {
+      // Create a state file reader for the initial state
+      EPMSHARED_PTR<StateFileReader<TSimType, LinearRotatingConvectionTraits> > pInState(new StateFileReader<TSimType, LinearRotatingConvectionTraits>(this->mCodC, this->mVelV,  "_initial"));
+
+      // Read in initial state
+      this->mIOSys.useInitialState(pInState, this->mSimControl.tsParams());
+
+      // Create a source file reader for the codensity source
+      EPMSHARED_PTR<SourceFileReader<TSimType, LinearRotatingConvectionTraits> > pSource(new SourceFileReader<TSimType, LinearRotatingConvectionTraits>(this->mCodC));
+
+      // Read in source state
+      this->mIOSys.useSource(pSource);
+
+      // Rescale codensity source to real value from parameters
+      this->mCodC.rOcSrc().rescale(this->mEqParams.codSourceScale());
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::addHDF5Output()
+   {
+      EPMSHARED_PTR<StateFileWriter<TSimType, LinearRotatingConvectionTraits> >  pOutState(new StateFileWriter<TSimType, LinearRotatingConvectionTraits>(this->mCodC, this->mVelV, this->mEqParams, this->mSimControl.tsParams()));
+
+      this->mIOSys.addHDF5Writer(pOutState);
+   }
+
+   template <typename TSimType> void LinearRotatingConvectionSimulation<TSimType>::addASCIIOutput()
+   {
+      // Create a timestep ASCII logging file
+      EPMSHARED_PTR<TimeFile> pTimeFile(new TimeFile("timestep", this->mSimControl.tsParams()));
+      // Add time file to ASCII output
+      this->mIOSys.addASCIIWriter(pTimeFile);
+
+      // Create a energy ASCII diagnostic file for the codensity scalar
+      EPMSHARED_PTR<EnergyFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::CodType> > pCodEnergy(new EnergyFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::CodType>(this->mCodC, "cod", this->mSimControl.tsParams()));
+      // Create a energy ASCII diagnostic file for the velocity field
+      EPMSHARED_PTR<EnergyFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::VelType> > pVelEnergy(new EnergyFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::VelType>(this->mVelV, "vel", this->mSimControl.tsParams()));
+
+      // Add codensity energy to ASCII output
+      this->mIOSys.addASCIIWriter(pCodEnergy);
+      // Add kinetic energy to ASCII output
+      this->mIOSys.addASCIIWriter(pVelEnergy);
+
+      // Create a energy spectrum ASCII diagnostic file for the codensity scalar
+      EPMSHARED_PTR<SpectrumFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::CodType> > pCodSpectrum(new SpectrumFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::CodType>(this->mCodC, "cod"));
+      // Create a energy spectrum ASCII diagnostic file for the velocity field
+      EPMSHARED_PTR<SpectrumFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::VelType> > pVelSpectrum(new SpectrumFile<TSimType, typename LinearRotatingConvectionTraits<TSimType>::VelType>(this->mVelV, "vel"));
+
+      // Add codensity energy spectrum to ASCII output
+      this->mIOSys.addASCIIWriter(pCodSpectrum);
+      // Add kinetic energy spectrum to ASCII output
+      this->mIOSys.addASCIIWriter(pVelSpectrum);
+   }
+
+}
+
+#endif // LINEARROTATINGCONVECTIONSIMULATION_HPP
