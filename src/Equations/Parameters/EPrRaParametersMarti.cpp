d8cd598834f61393f6a0569d709e8264acb40755
Leonardo Echeverria <leonardo.echeverria@gmail.com>
2016-07-05 15:30:49 +0200

    Adding multile parameters files

--- src/Equations/Parameters/EPrRaParametersMarti.cpp
+++ src/Equations/Parameters/EPrRaParametersMarti.cpp
@@ -1,6 +1,7 @@
 /** \file EPrRaParameters.cpp
  *  \brief Source of the implementation of the equation non dimensional parameters
  */
+//Leo: Parameters for Marti's Benchmark
 
 // System includes
 //
